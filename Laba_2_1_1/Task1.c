9396d917717194d691a735d6d684c343579146b4
Hardicine <hardicine@mail.ru>
2025-03-09 23:25:30 +0300

    09.03 23:25

--- Laba_2_1_1/Task1.c
+++ Laba_2_1_1/Task1.c
@@ -9,7 +9,7 @@
 // - определить количество самолетов, грузоподъемность 
 // которых меньше заданной
 
-struct Plain
+struct Plane
 {
 	int number;
 	char mark[10];
@@ -42,57 +42,63 @@ double input_double(char query[])
 	return number;
 }
 
+void Input(struct Plane planes[], int plane_count)
+{
+	for (int i = 0; i < plane_count; i++)
+	{
+		puts("================================================");
+		printf_s("Самолёт #%d\n", i + 1);
+		planes[i].number = input_int("Введите бортовой номер:");
+		printf_s("Введите марку: ");
+		scanf("%s", &planes[i].mark);
+		planes[i].capacity = input_int("Введите вместимость:");
+		planes[i].weight = input_double("Введите грузоподъёмность:");
+		planes[i].year = input_int("Введите год выпуска:");
+	}
+}
+
+void Output(struct Plane planes[], int plane_count)
+{
+	puts("================================================================================================");
+	puts("|| # || Бортовой номер || Вместимость || Грузоподъемность || Год выпуска ||       Марка       ||");
+	puts("================================================================================================");
+
+	for (int i = 0; i < plane_count; i++)
+	{
+		printf_s("||%2d || %9d      || %7d     || %10.2lf       || %8d    || %10s        || \n", \
+			i + 1, planes[i].number, planes[i].capacity, planes[i].weight, planes[i].year, planes[i].mark);
+	}
+
+	puts("================================================================================================");
+}
+
+double Max_Weight_Find(struct Plane planes[], int plane_count)
+{
+	double temp = planes[0].weight;
+	for (int i = 1; i < plane_count; i++)
+	{
+		planes[i].weight > temp ? temp = planes[i].weight : temp;
+	}
+	return temp;
+}
+
 void main()
 {
 	setlocale(LC_ALL, "rus");
 	SetConsoleCP(1251);
 	SetConsoleOutputCP(1251);
 
-	struct Plain plains[MAX_SIZE];
+	struct Plane planes[MAX_SIZE];
 
-	int size = input_int("Введите количество самолётов:");
+	int plane_count = input_int("Введите количество самолётов:");
+	Input(planes, plane_count);
+	Output(planes, plane_count);
 
-	for (int i = 0; i < size; i++)
+	double max_weight_value = Max_Weight_Find(planes, plane_count);
+	struct Plane planes_max_weight[MAX_SIZE];
+	for (int i = 0, j = 0; i < plane_count; i++)
 	{
-		puts("================================================");
-		printf_s("Самолёт #%d\n", i + 1);
-		plains[i].number = input_int("Введите бортовой номер:");
-		printf_s("Введите марку: ");
-		scanf("%s", &plains[i].mark);
-		plains[i].capacity = input_int("Введите вместимость:");
-		plains[i].weight = input_double("Введите грузоподъёмность:");
-		plains[i].year = input_int("Введите год выпуска:");
-	}
-
-	puts("================================================================================================");
-	puts("|| # || Бортовой номер || Вместимость || Грузоподъемность || Год выпуска ||       Марка       ||");
-	puts("================================================================================================");
-
-	for (int i = 0; i < size; i++)
-	{
-		printf_s("||%2d || %9d      || %7d     || %10.2lf       || %8d    || %10s        || \n", \
-			i + 1, plains[i].number, plains[i].capacity, plains[i].weight, plains[i].year, plains[i].mark);
-	}
-
-	puts("================================================================================================");
-
-	double temp;
-
-	for (int i = 1; i < size; i++)
-	{
-		temp = plains[0].weight;
-		if (plains[i].weight > temp)
-		{
-			temp = plains[i].weight;
-		}
-	}
-
-	int max_weight[MAX_SIZE];
-	int max_weight_size = 0;
-
-	for (int i = 0, j = 0; i < size; i++)
-	{
-		if (plains[i].weight == temp)
+		if (planes[i].weight == max_weight_value)
 		{
 			max_weight[j] = i;
 			j++;
@@ -104,15 +110,15 @@ void main()
 
 	for (int i = 0; i < max_weight_size; i++)
 	{
-		printf_s("#%d, ", max_weight[i] + 1);
+		printf_s("#%d; ", max_weight[i] + 1);
 	}
 	puts("");
 
 	int flag = 0;
 	double weight_input = input_double("Введите грузоподъёмность, по которой хотите провести сравнение: ");
-	for (int i = 0; i < size; i++)
+	for (int i = 0; i < plane_count; i++)
 	{
-		plains[i].weight < weight_input ? flag++ : flag;
+		planes[i].weight < weight_input ? flag++ : flag;
 	}
 
 	printf_s("Количество самолётов, у которых грузоподъёмность меньше %.2lf равно %d\n", weight_input, flag);
